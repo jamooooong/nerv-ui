bcc1ec5e0c898fd990e8987bccd75c7e3f38aaf7
jamooooong <102500033+jamooooong@users.noreply.github.com>
2025-01-19 00:44:32 +0900

    🎉 start nerv-ui

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as icons from "./shared/assets/icons";
+import "./shared/styles.css";
+
+export { icons };
+
+export * from "./components/Button";
+export * from "./Icon";
+//export * from "./components/TwTest";
